8398d714cdaacfb0841a39c327d9de2625cdcd49
Harsh Bhardwaj <hbhardwaj@ptc.com>
2018-08-11 23:27:59 +0530

    expedia challenge

--- Optmyzr-task/server.js
+++ Optmyzr-task/server.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, function () {
+  console.log("Server is up and running");
+});
+
